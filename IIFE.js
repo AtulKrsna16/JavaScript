7a5f0bb5d2ab4b3582aea2eb4ccf328d951d51a8
Atul Kumar <105835497+AtulKrsna16@users.noreply.github.com>
2023-10-19 17:25:58 +0530

    done

--- IIFE.js
+++
@@ -1,7 +0,0 @@
-(function hello () {
-    console.log("hello Atul");
-})();
-
-((name) => {
-    console.log(`Hi ${name} welcome to IIFE function`)
-})("Atul")
--- object.js
+++
@@ -1,8 +0,0 @@
-const course = {
-    course : "js in hindi",
-    price : "999",
-    courseinstructor : "hitesh"
-}
-// console.log(course.courseinstructor);
-const {courseinstructor : instructor} = course;
-console.log(instructor);
